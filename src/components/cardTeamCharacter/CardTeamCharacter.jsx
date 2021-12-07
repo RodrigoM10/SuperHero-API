9e4dc8354daf3ac4cf19c57ee9fcacf6f1c2f87b
Rodrigo <rodrigomendozar10@gmail.com>
2021-12-07 17:07:50 -0300

    powerstats

--- src/components/cardTeamCharacter/CardTeamCharacter.jsx
+++ src/components/cardTeamCharacter/CardTeamCharacter.jsx
@@ -1,26 +1,31 @@
 import React from 'react'
-import { Card } from 'react-bootstrap'
+import { Card, ListGroup } from 'react-bootstrap'
 
-export const CardTeamCharacter = ({teamChar, team, setTeam}) => {
+export const CardTeamCharacter = ({ teamChar, team, setTeam }) => {
+    console.log("🚀 ~ file: cardTeamCharacter.jsx ~ line 5 ~ CardTeamCharacter ~ team", team)
+
+    const {character} = teamChar;
 
     const removeToTeam = () => {
         const filterTeam = team.filter((char) => char.character.id !== teamChar.character.id);
         setTeam(filterTeam);
-      };
+    };
 
     return (
         <Card className="card-style p-0 mx-3">
-            <Card.Img className="card-img-style " variant="top" src={teamChar.character.image.url} />
+            <Card.Img className="card-img-style " variant="top" src={character.image.url} />
             <Card.Body>
-                <Card.Title>{teamChar.name}</Card.Title>
-                <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                </Card.Text>
+                <Card.Title>{character.name}</Card.Title>
+                <ListGroup variant="flush">
+                    <ListGroup.Item>Combate: {character.powerstats.combat} | Durabilidad: {character.powerstats.durability}</ListGroup.Item>
+                    <ListGroup.Item>Inteligencia: {character.powerstats.intelligence} | Poder: {character.powerstats.power}</ListGroup.Item>
+                    <ListGroup.Item>Velocidad: {character.powerstats.speed} | Fuerza: {character.powerstats.strength}</ListGroup.Item>
+                    <ListGroup.Item></ListGroup.Item>
+                </ListGroup>
             </Card.Body>
             <div>
                 <button onClick={removeToTeam}>
-                  Eliminar
+                    Eliminar
                 </button>
                 <button className="" >
                     Detalle
